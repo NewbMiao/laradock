feca65ea0824a35b3521a5bfe865cc9a26315b61
newbmiao <newbvirgil@gmail.com>
2017-06-22 17:44:32 +0800

    add go

--- go/init.sh
+++ go/init.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-export TASK_DIR=/tmp/task
+export TASK_DIR=/tmp/$EXENAME
 mkdir $TASK_DIR
 rm -rf $TASK_DIR/*
-bee pack -p /go/src/task  -exr="^[0-9a-f]$" -o  $TASK_DIR
+bee pack -p /go/src/$EXENAME  -exr="^[0-9a-f]$" -o  $TASK_DIR
 cd $TASK_DIR
-tar -zxf task.tar.gz
-./task
+tar -zxf $EXENAME.tar.gz
+./$EXENAME
 
